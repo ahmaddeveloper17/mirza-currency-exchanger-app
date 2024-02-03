661c37ab8a43d73db7416662a99162257a665f06
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-02-03 16:38:05 +0500

    mirza

---
+++ src/components/card.tsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import arrow from "../assets/images-removebg-preview.png"
+interface CardProps {
+  description: string;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  const location = useLocation();
+  const currency = new URLSearchParams(location.search).get('currency');
+  const { description } = props;
+
+  // Condition for not showing the result
+  const shouldNotShowResult = currency === description;
+
+  return (
+    <div className='text-black'>
+      {!shouldNotShowResult && (
+        <div className="ml-[20px] mt-[50px]  flex flex-col sm:flex-row bg-gray-100 p-8 w-[250px]">
+          <p className="text-lg font-bold">{currency}</p>
+          <div className="w-[40px] mt-[5px] mx-[14px]">
+            <img src={arrow} alt="" />
+          </div>
+          <p className="text-lg font-bold">{description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/pages/Final.tsx
@@ -0,0 +1,23 @@
+import CurrencyCard from '.././components/currencyCard'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function Final() {
+    const location = useLocation();
+    console.log('Location:', location);
+  
+    const CUR1 = new URLSearchParams(location.search).get('CUR1');
+    const toCUR2 = new URLSearchParams(location.search).get('toCUR2');
+  return (
+    <div className='mt-20 max-w-screen '>
+    <div className="bg-blue-100 h-[300px]">
+    <h1 className="text-center text-[32px] font-roboto text-gray-900 text-6xl font-bold pt-10 sm:pt-[42px]">
+        Convert {CUR1} to {toCUR2}
+      </h1>
+    </div>
+      <div className="mt-[-126px] overflow-x-hidden pb-[108px]">
+        <CurrencyCard />
+      </div>
+    </div>
+  )
+}
